bda7ecac847dea6451324f0eb37f055e27b7457d
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-08-30 21:17:56 +0530

    Update sonarscan.groovy

--- vars/sonarscan.groovy
+++ vars/sonarscan.groovy
@@ -1,7 +1,5 @@
 def call(){
-        steps { 
         withSonarQubeEnv('sonar') { // If you have configured more than one global server connection, you can specify its name
             sh "${scannerHome}/bin/sonar-scanner"
     }
   }
-}
