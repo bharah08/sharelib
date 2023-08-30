cc22cc2b0cf240099d251ffefa5e4e4d3045a993
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-08-30 21:57:34 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,4 +1,9 @@
 def call(){
- sh ' dockerImage = docker.build registry + ":$BUILD_NUMBER" '
+ sh """ 
+ dockerImage = docker.build registry + ":$BUILD_NUMBER"
+ docker.withRegistry( '', registryCredential ) {          
+ dockerImage.push("$BUILD_NUMBER")
+ """
+}
 
         }
