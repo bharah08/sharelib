5409a5f0368c8db350f9144f0e3a085d022caebd
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-11-10 10:24:32 +0530

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,3 +1,3 @@
 def call(){
-  git 'https://github.com/bharah08/ecom.git'
+  checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/bharah08/Train-Ticket-Reservation-System.git']])
 }
